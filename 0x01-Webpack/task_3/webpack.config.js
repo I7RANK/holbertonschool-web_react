42ca7c699a3819ae9fe6a82b7573ff1293b26860
I7RANK <franfgh2000@hotmail.com>
2021-11-07 12:51:48 -0500

    Uploading task_3

---
+++ 0x01-Webpack/task_3/webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    header: './modules/header/header.js',
+    body: './modules/body/body.js',
+    footer: './modules/footer/footer.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  plugins: [new HtmlWebpackPlugin()],
+  devServer: {
+    static: path.join(__dirname, './public'),
+    compress: true,
+    port: 8564,
+  },
+  optimization: {
+    runtimeChunk: 'single'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+    ]
+  }
+};
