eb83ce9fd0a27d8da2580f5825bb2dc103a4b0ca
I7RANK <franfgh2000@hotmail.com>
2021-11-06 22:53:31 -0500

    Uploading task 2

---
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,22 @@
+import $ from 'jquery';
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button id="click-here">Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+const counter = (function () {
+  let counter = 0;
+
+  return function () {
+    counter++;
+    return counter;
+  };
+})();
+
+function updateCounter() {
+  $('#count').text(counter() + ' clicks on the button');
+}
+
+$('#click-here').on('click', updateCounter);
