3d1fee3ea7d602636b41bb4f4518fad3043cdd69
I7RANK <franfgh2000@hotmail.com>
2021-12-01 12:16:38 -0500

    Pinche checker y el que lo hizo

--- 0x07-React_Immutable/4-mutations.js
+++ 0x07-React_Immutable/4-mutations.js
@@ -9,7 +9,6 @@ export const map = Map({
   6: 'Lucas',
 });
 
-export const map2 = map.withMutations(function (map) {
-  map.set(2, 'Benjamin');
-  map.set(4, 'Oliver');
+export const map2 = map.withMutations((mapItem) => {
+  mapItem.set(2, 'Benjamin').set(4, 'Oliver');
 });
